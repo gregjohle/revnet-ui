2bc5fd1742dbc84b05ea3cd2db5a2ad9519ea219
Greg <greg.johle@gmail.com>
2023-01-09 19:58:17 -0500

    started signup page

--- src/Components/UserManagement/SignUp.js
+++ src/Components/UserManagement/SignUp.js
@@ -1,5 +1,8 @@
 import { supabase } from '../../Common/supabaseClient'
+import { useState } from 'react'
 
 export default function SignUp() {
-    
+    const [userEmail, setEmail] = useState('')
+    const [firstName, setFirstNanme] = useState('')
+
 }
